163593547096279b2a331f29aadbaa38cd87725a
lihini223 <lihininisansala95@gmail.com>
2021-09-04 16:03:35 +0530

    Retrieve movie details one by one

--- pick_a_movie.py
+++ pick_a_movie.py
@@ -14,4 +14,14 @@ print(type(html_soup))
 
 movie_containers = html_soup.findAll('div', class_='lister-item mode-advanced')
 
+print(type(movie_containers))
+print(len(movie_containers))
 
+first_movie = movie_containers[0]
+first_name = first_movie.h3.a.text
+first_year = (first_movie.h3.find(
+    'span', class_='lister-item-year text-muted unbold')).text
+first_imdb = float(first_movie.strong.text)
+first_mscore = (first_movie.find('span', class_='metascore favorable')).text
+first_votes = first_movie.find('span', attrs = {'name':'nv'})
+print(first_mscore)
